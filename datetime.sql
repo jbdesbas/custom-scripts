53a344879c9f8166abd82923cedd2633f9fc221a
JB Desbas <jb.desbas@gmail.com>
2021-06-16 12:28:09 +0200

    is_date_in_period

---
+++ datetime.sql
@@ -0,0 +1,20 @@
+CREATE OR REPLACE FUNCTION is_date_in_period(input_date date, doy_start smallint, doy_stop smallint )
+RETURNS boolean
+ LANGUAGE plpgsql
+ IMMUTABLE
+AS $function$
+--TRUE si la date est situé dans la période entre les deux "days of year". Fonctionne aussi pour les périodes à cheval sur deux années (doy_stop < doy_start)
+--Return NULL si doy_star = doy_stop (impossible de déterminer la période à couvrir)
+  BEGIN
+    IF doy_start < doy_stop THEN
+      return (SELECT input_date BETWEEN (date_trunc('year',input_date) + (doy_start-1||' days')::INTERVAL)::date 
+     			AND (date_trunc('year',input_date) + (doy_stop-1||' days')::INTERVAL)::date);
+    ELSIF doy_start > doy_stop THEN
+      RETURN (SELECT input_date >= (date_trunc('year',input_date) + (doy_start-1||' days')::INTERVAL)::date
+     			OR input_date <= (date_trunc('year',input_date) + (doy_stop-1||' days')::INTERVAL)::date);
+    ELSE 
+    	RETURN null;
+    END IF;
+  END;
+$function$
+;
