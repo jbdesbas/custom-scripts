3f145c3e87165e484c7d721f6b620d0008a0415d
Jean-Baptiste DESBAS <jean-baptiste.desbas@hautsdefrance.fr>
2023-07-20 16:33:00 +0200

    serie type (bar, line, etc.)

--- dataviz.sql
+++ dataviz.sql
@@ -6,7 +6,7 @@ $$
 -- Jean-Baptiste DESBAS, 2023 jb@desbas.fr
 -- Passer en argument le nom d'une table (ou vue ou table temporaire)
 -- La table doit avoir un champs x et y
--- Champs supportés : x, y, stack, serie
+-- Champs supportés : x, y, stack, serie, type
 DECLARE myout TEXT;
 DECLARE opt jsonb;
 DECLARE jsdata jsonb;
@@ -17,14 +17,14 @@ s record;
 BEGIN	
 	--TODO ajouter les colonnes manquantes avec un NATURAL FULL JOIN
 	js_series_array = '[]'::jsonb;
-	FOR s IN EXECUTE(FORMAT('SELECT DISTINCT serie, stack FROM %I', _tbl) )
+	FOR s IN EXECUTE(FORMAT('SELECT DISTINCT ON(serie) serie, stack, type FROM %I', _tbl) )
 	LOOP
 		jsdata = '[]'::jsonb;
 		FOR e IN EXECUTE(FORMAT('SELECT x, y, serie FROM %I WHERE serie=%L ORDER BY x', _tbl, s.serie) )
 		LOOP
 			jsdata = jsdata || jsonb_build_array(jsonb_build_array(e.x, e.y));
 		END LOOP;
-		js_serie = jsonb_build_object('name',s.serie, 'stack', s.stack, 'type', 'bar', 'data', jsdata);
+		js_serie = jsonb_build_object('name',s.serie, 'stack', s.stack, 'type', s."type", 'data', jsdata);
 		js_series_array = js_series_array || js_serie;
 	END LOOP;
 	opt = jsonb_build_object(
@@ -37,3 +37,4 @@ BEGIN
 	 RETURN opt;
 END;
 $$;
+
