e030e5f3daf406266ffcdc00aeb5a40695fac5fd
Jean-Baptiste DESBAS <jean-baptiste.desbas@hautsdefrance.fr>
2023-07-20 15:53:04 +0200

    support series

--- dataviz.sql
+++ dataviz.sql
@@ -10,25 +10,31 @@ $$
 DECLARE myout TEXT;
 DECLARE opt jsonb;
 DECLARE jsdata jsonb;
+DECLARE js_series_array jsonb;
+DECLARE js_serie jsonb;
 e record;
+s record;
 BEGIN	
-	jsdata = '[]'::jsonb;
-	FOR e IN EXECUTE(FORMAT('SELECT x, y FROM %I ORDER BY x', _tbl) )
+	--TODO ajouter les colonnes manquantes avec un NATURAL FULL JOIN
+	js_series_array = '[]'::jsonb;
+	FOR s IN EXECUTE(FORMAT('SELECT DISTINCT serie FROM %I', _tbl) )
 	LOOP
-		jsdata = jsdata || jsonb_build_array(jsonb_build_array(e.x, e.y));
+		jsdata = '[]'::jsonb;
+		RAISE INFO '%', (FORMAT('SELECT x, y, serie FROM %I WHERE serie=%L ORDER BY x', _tbl, s.serie) );
+		FOR e IN EXECUTE(FORMAT('SELECT x, y, serie FROM %I WHERE serie=%L ORDER BY x', _tbl, s.serie) )
+		LOOP
+			RAISE INFO '%', e; 
+			jsdata = jsdata || jsonb_build_array(jsonb_build_array(e.x, e.y));
+		END LOOP;
+		js_serie = jsonb_build_object('name',s.serie, 'type', 'bar', 'data', jsdata);
+		js_series_array = js_series_array || js_serie;
 	END LOOP;
-	
 	opt = jsonb_build_object(
 	'xAxis', 
 		jsonb_build_object('type','time'),
 	'yAxis',
 		jsonb_build_object('type','value') ,
-	'series', jsonb_build_array(
-		jsonb_build_object('type','bar',
-			'data',
-				jsdata
-			) 
-		) 
+	'series', js_series_array
 	);
 	 RETURN opt;
 END;
