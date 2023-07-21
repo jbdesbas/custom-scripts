e762479c8777d7d6c872fbe83b1d1474be534cbd
Jean-Baptiste DESBAS <jean-baptiste.desbas@hautsdefrance.fr>
2023-07-21 10:12:47 +0200

    dataviz page

--- dataviz.sql
+++ dataviz.sql
@@ -43,3 +43,42 @@ BEGIN
 	 RETURN opt;
 END;
 $$;
+
+CREATE OR REPLACE FUNCTION dataviz_page(charts_opt_arr _jsonb)
+RETURNS text 
+LANGUAGE plpgsql
+AS 
+$function$
+-- Jean-Baptiste DESBAS, 2023 jb@desbas.fr
+-- Génère une page html prête à l'emploi
+-- Passer en argument un tableau (_jsonb) contenant les options des graphiques (générés avec dataviz())
+DECLARE 
+	i INTEGER := 0;
+	dataviz_content TEXT := '';
+	c jsonb;
+BEGIN 
+	FOREACH c IN ARRAY charts_opt_arr LOOP 
+		dataviz_content := concat(dataviz_content,format('<div id="chart_%1$s" style="width: 600px; height: 400px;"></div>
+				<script>
+			        const chart_%1$s = echarts.init(document.getElementById("chart_%1$s"));
+					chart_%1$s.setOption(%2$s)
+				</script>', i::TEXT, c::text ));
+		i := i + 1; 
+	END LOOP;
+	
+	RETURN format('<!DOCTYPE html>
+	<html>
+	<head>
+	    <meta charset="utf-8">
+	    <title>Graphique Apache ECharts</title>
+	    <!-- Charger la bibliothèque ECharts depuis un CDN -->
+	    <script src="https://cdn.jsdelivr.net/npm/echarts@5.1.2/dist/echarts.min.js"></script>
+	</head>
+	<body>
+	%s
+	</body>
+	</html>',dataviz_content);
+END;
+$function$;
+
+
